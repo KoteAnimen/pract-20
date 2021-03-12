2cb7f3061af1aafde0d00e47c7bdd6b74558903d
KoteAnimen <sverh.il@yandex.ru>
2021-03-12 10:40:42 +0300

    Добавлены формы: Добавить, Изменить, Просмотр

---
+++ pract-20/ChangeRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pract_20
+{
+    public partial class ChangeRecord : Form
+    {
+        public ChangeRecord()
+        {
+            InitializeComponent();
+        }
+
+        private void unitsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.unitsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.unitsAccountDataSet);
+
+        }
+
+        private void AddRecord_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "unitsAccountDataSet.Units". При необходимости она может быть перемещена или удалена.
+            this.unitsTableAdapter.Fill(this.unitsAccountDataSet.Units);
+
+        }
+    }
+}
